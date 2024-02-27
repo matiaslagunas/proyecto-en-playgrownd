62f654efa456a8247e4b70ea4a809f331198aa93
Matias Lagunas <matias.lagunas.t@gmail.com>
2024-02-27 20:41:36 -0300

    cargado de trabajo mercadoLiebre

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const port = 3030;
+const viewsPath = path.join(__dirname, 'view');
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log("El servidor está corriendo en el puerto " + port);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'home.html'));
+});
+
+app.get('/view/login.html', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'login.html'));
+});
+
+app.post('/view/login.html', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'home.html'));
+});
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'register.html'));
+});
+
+app.post('/register.html', (req, res) => {
+    res.sendFile(path.join(viewsPath, 'home.html'));
+});
+
