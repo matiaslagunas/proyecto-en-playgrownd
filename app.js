3afd8b009523397682a3be940353b118c53bb5b3
Matias Lagunas <matias.lagunas.t@gmail.com>
2024-02-28 00:09:42 -0300

    funciona por favor

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const {resolve} = require('path');
 const port = process.env.PORT || 3030;
-const viewsPath = path.join(__dirname, 'views');
+
 
 app.use(express.static('public'));
 
@@ -12,25 +12,18 @@ app.listen(port, () => {
 
 
 
-
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(viewsPath, '/views/home.html'));
+    res.sendFile(resolve('./views/home.html'))
 });
 
 app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(viewsPath, '/views/login.html'));
+    res.sendFile(resolve('./views/login.html'));
 });
 
-app.post('/login.html', (req, res) => { 
-    res.sendFile(path.join(viewsPath, '/views/home.html'));
-});
 
 app.get('/register.html', (req, res) => {
-    res.sendFile(path.join(viewsPath, '/views/register.html'));
+    res.sendFile(resolve('./views/register.html'));
 });
 
-app.post('/register.html', (req, res) => {
-    res.sendFile(path.join(viewsPath, '/views/home.html'));
-});
+
 
